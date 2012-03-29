6684b475c92d692266affdb2fa488dca80bcd047
Ben Woodall <benwoody@gmail.com>
2012-03-29 14:32:58 -0700

    removed extranious crap. added comments

--- lib/splunk.rb
+++ lib/splunk.rb
@@ -7,20 +7,19 @@ module Splunk
   require 'hpricot'
 
   class Auth
+
     HOST = 'tpsplunk'
     PORT = 8089
 
-    def splunk_ssl_post_request(path, data = nil, headers = nil)
+    # Initial SSL request
+    def splunk_ssl_request(path, data = nil, headers = nil)
       http = Net::HTTP.new(HOST, PORT)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.post(path, data, headers).body
     end
 
-    def session_key
-      @session_key ||= token
-    end
-
+    # Grab token with username and password
     def token(user, pass)
       doc = Hpricot(splunk_ssl_post_request("/services/auth/login", "username=#{user}&password=#{pass}"))
       (doc/"//sessionkey").inner_html
